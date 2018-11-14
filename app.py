4d717f59620e1fa055274aa46dddcc96969d02ba
vctrout1 <43351113+vctrout1@users.noreply.github.com>
2018-11-14 09:11:28 -0500

    Initial Version - Bad Request when try to add new record

--- app.py
+++ app.py
@@ -14,37 +14,44 @@ def sendSQL(sql):
 
 sendSQL(sql)
 
-def getData():
+def getData(a):
     db = sqlite3.connect("./directory.db")
     cur = db.cursor()
-    sql = 'SELECT * FROM directory'
-    cur.execute(sql)
+    cur.execute(a)
     db.commit()
     results = cur.fetchall()
     db.close()
+    print(results)
+    patient=results
     return results
 
-
 app = Flask(__name__)
-
-@app.route('/add', methods=['GET','POST'])
-def add():
-    if request.method =="POST":
-        sql ='''INSERT INTO directory (OwnerLast, OwnerFirst, PetName, PetType, Notes) VALUES ('{}','{}','{}','{}','{}')'''.format(request.form["ownerLast"],request.form["ownerFirst"],request.form["petName"],request.form["petType"],request.form["note"])
-        print(sql)
-        sendSQL(sql)
-    patientList = getData()
-    return render_template("add.html", patientList=patientList)
-
 
 @app.route('/')
 def home():
     return render_template("home.html")
 
-@app.route('/manage', methods=['GET','POST'])
-def directory():
-    directory = getData()
-    return render_template("manage.html",directory=directory)
+@app.route('/add', methods=['GET','POST'])
+def add():
+    if request.method =="POST":
+        sql ='''INSERT INTO directory (OwnerLast, OwnerFirst, PetName, PetType, Notes) VALUES ('{}','{}','{}','{}','{}')'''.format(request.form["ownerLast"],request.form["ownerFirst"],request.form["petName"],request.form["petType"],request.form["note"])
+#sql ='''INSERT INTO directory (OwnerLast, OwnerFirst, PetName, PetType, Notes) VALUES ("Carr","Betty","Blackberry","Cat","needs an appt")'''
+        print(sql)
+        sendSQL(sql)
+#patientList = getData()
+    return render_template("add.html")
 
-if __name__ == '__main__':
+@app.route('/manage', methods=['GET','POST'])
+def manage():
+    if request.method == "GET":
+
+        lookup = '''SELECT * FROM directory WHERE PetName = "Maggie"'''
+        getData(lookup)
+        all='''SELECT * FROM directory'''
+        directory=getData(all)
+
+    return render_template("manage.html", directory=directory)
+
+
+if __name__ == "__main__":
     app.run()
